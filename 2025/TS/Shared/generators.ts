575d126ddedaf537e63f37ca762d819d828b59e6
Jason Tucker <jtgeek@gmail.com>
2025-12-05 22:01:41 -0500

    Further manual migration

--- 2025/TS/Shared/generators.ts
+++ 2025/TS/Shared/generators.ts
@@ -1,9 +1,8 @@
-import { asyncReduce } from 'iter-tools'
+import { asyncReduce, AsyncWrappable } from 'iter-tools'
 
-export type AnyGenerator<T> = Iterable<T> | AsyncIterable<T>
 
-export const reduce = async <TAcc, TCurr>(f: (acc: TAcc, curr: TCurr) => TAcc, i: TAcc, it: AnyGenerator<TCurr>) =>
+export const reduce = async <TAcc, TCurr>(f: (acc: TAcc, curr: TCurr) => TAcc, i: TAcc, it: AsyncWrappable<TCurr>) =>
    await asyncReduce(i, f, it)
 
-export const sum = async <T>(it: AnyGenerator<T>, f: (s: T) => number) =>
+export const sum = async <T>(it: AsyncWrappable<T>, f: (s: T) => number) =>
    await asyncReduce(0, (acc: number, curr: T): any => acc + f(curr), it)
